6d789eee355f0012ea93cd63bbe3714c21774ac9
Gabriela johnson <johnsongabriela12@gmail.com>
2020-07-14 01:25:16 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -18,3 +18,22 @@ class String
 
   end
 end
+
+class String
+
+  def sentence?
+    self.end_with?(".")
+  end
+
+  def question?
+    self.end_with?("?")
+  end
+  
+  def exclamation?
+    self.end_with?("!")
+  end
+  
+  def count_sentences
+    self.split(/[.!?]/).reject {|x| x.empty?}.size
+  end
+  
